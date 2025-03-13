6c57d2a89f4b7640649e166710f0a9414b410955
kla <9581117+lighthaus205@users.noreply.github.com>
2025-03-13 09:59:32 +0100

    fix height for mobile devices

--- app/[nftAddress]/page.tsx
+++ app/[nftAddress]/page.tsx
@@ -5,7 +5,7 @@ import ShaderExperience from "@/app/lib/components/ShaderExperience";
 
 export default function Page({ params }: { params: { nftAddress: string } }) {
   return (
-    <div className="items-center justify-items-center min-h-screen w-screen h-screen">
+    <div className="items-center justify-items-center min-h-dvh w-screen h-dvh">
       <ShaderExperience nftAddress={params.nftAddress} />
     </div>
   );
