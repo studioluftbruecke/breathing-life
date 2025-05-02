574376ef9f2fc0c4f43893b07652b37194b0db05
konchord <206325195+studioluftbruecke@users.noreply.github.com>
2025-05-02 16:57:47 +0200

    npm run build fixes

--- lib/components/ShaderExperience.tsx
+++ lib/components/ShaderExperience.tsx
@@ -3,7 +3,7 @@
 import ExperienceWrapper from "@/lib/components/ExperienceWrapper";
 import * as THREE from 'three'
 import { ToastContainer } from 'react-toastify';
-import { BreathingLifePlane } from "./BreathingLifePlane";
+import { BreathingLifePlaneWrapper } from "./BreathingLifePlane";
 import dynamic from "next/dynamic";
 import { useShaderSettings } from "../stores/useShaderSettings";
 
@@ -24,7 +24,7 @@ export default function ShaderExperience() {
         cameraFov={75}
       >
         <ambientLight />
-        <BreathingLifePlane
+        <BreathingLifePlaneWrapper
           settings={{
             mixNoise,
             worleyNoiseScale,
