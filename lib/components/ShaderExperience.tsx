088ed6891110b6c2b7669cb13617333e832f0e4d
konchord <206325195+studioluftbruecke@users.noreply.github.com>
2025-05-03 22:29:55 +0200

    Implement Re-center canvas functionality

--- lib/components/ShaderExperience.tsx
+++ lib/components/ShaderExperience.tsx
@@ -6,23 +6,35 @@ import { ToastContainer } from 'react-toastify';
 import { BreathingLifePlaneWrapper } from "./BreathingLifePlane";
 import dynamic from "next/dynamic";
 import { useShaderSettings } from "../stores/useShaderSettings";
+import { Button } from "./ui/button";
 
 
 const ShaderSettingsInterface = dynamic(() => import('./ShaderSettingsInterface'), { ssr: false })
-
+const initCameraPosition = new THREE.Vector3(0, 0, 1)
 
 export default function ShaderExperience(props: {
   userHasAccess: boolean
 }) {
-  const { mixNoise, worleyNoiseScale, simplexNoiseScale, simplexSpeed, simplexIntensity, worleySpeed, worleyIntensity, image } = useShaderSettings()
+  const { mixNoise, worleyNoiseScale, simplexNoiseScale, simplexSpeed, simplexIntensity, worleySpeed, worleyIntensity, image, cameraPosition, triggerReset, setCameraPosition, setTriggerReset } = useShaderSettings()
 
   return (
     <>
+      {new THREE.Vector3().copy(cameraPosition).sub(initCameraPosition).length() > 0.01 && (
+        <div className="fixed bottom-2 z-50">
+          <Button
+            variant={'ghost'}
+            className={`border rounded-md flex items-center justify-center`}
+            onClick={() => { setTriggerReset(Math.random().toString()) }}
+          >
+            <span>Re-center</span>
+          </Button>
+        </div>
+      )}
       <ToastContainer />
       <ShaderSettingsInterface userHasAccess={props.userHasAccess} />
       <ExperienceWrapper
         controls={{ orbitControls: {} }}
-        initialCameraPosition={new THREE.Vector3(0, 0, 1)}
+        initialCameraPosition={initCameraPosition}
         cameraFov={75}
       >
         <ambientLight />
@@ -35,7 +47,9 @@ export default function ShaderExperience(props: {
             simplexIntensity,
             worleySpeed,
             worleyIntensity,
-            image
+            image,
+            triggerReset,
+            setCameraPosition
           }}
         />
       </ExperienceWrapper>
