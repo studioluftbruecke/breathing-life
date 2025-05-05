c8b98fa8c15ed01f01aa7224191f0bb2b23f9b5e
konchord <206325195+studioluftbruecke@users.noreply.github.com>
2025-05-05 12:58:50 +0200

    use Info Icon in bottom-left corner instead of SL-logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { Separator } from "../lib/components/ui/separator";
 import { useUpProvider } from "../lib/providers/UpProvider";
 
 import {
@@ -31,8 +30,9 @@ import { lsp7DigitalAssetAbi } from '@lukso/lsp-smart-contracts/abi';
 import { useProfile } from '@/lib/providers/ProfileProvider';
 import Image from 'next/image';
 import Link from 'next/link';
+import { Button } from '@/lib/components/ui/button';
+import { Info } from 'lucide-react';
 const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_LSP7_CONTRACT_ADDRESS;
-const MAX_SUPPLY = Number(process.env.NEXT_PUBLIC_LSP7_CONTRACT_MAX_SUPPLY ?? 100);
 
 
 export default function Home() {
@@ -40,8 +40,6 @@ export default function Home() {
   const { isMiniApp, accounts } = useUpProvider();
   const [isClient, setIsClient] = useState(false);
   const [userHasAccess, setUserHasAccess] = useState(false);
-  const [tokenBalance, setTokenBalance] = useState<number>();
-  const [totalSupply, setTotalSupply] = useState<number>();
 
   useEffect(() => {
     console.log('Setting isClient to true');
@@ -57,7 +55,6 @@ export default function Home() {
           functionName: 'totalSupply',
         });
         console.log('totalSupply', totalSupply);
-        setTotalSupply(Number(totalSupply));
       } catch (error) {
         console.error('Error fetching total supply:', error);
         throw error;
@@ -69,8 +66,6 @@ export default function Home() {
 
   useEffect(() => {
     const fetchOffForNewShoresTestBalance = async (address: Address) => {
-      // Contract address of Off For New Shores - Test
-
       // Fetch balance
       const balanceResult = await publicClient.readContract({
         address: CONTRACT_ADDRESS as Address,
@@ -79,7 +74,6 @@ export default function Home() {
         args: [address],
       });
       const balance = Number(balanceResult);
-      setTokenBalance(balance);
       setUserHasAccess(balance > 0);
     }
 
@@ -87,7 +81,6 @@ export default function Home() {
       fetchOffForNewShoresTestBalance(accounts[0]);
     } else {
       console.warn('No accounts found');
-      setTokenBalance(0);
       setUserHasAccess(false);
     }
   }, [accounts])
@@ -104,57 +97,31 @@ export default function Home() {
         <ShaderExperience
           userHasAccess={userHasAccess}
         />
-        {/* {!accountHasAccess ? <>
-          {!profileData && <>
-            <div className='flex flex-col'>
-              <div className='flex flex-row items-center mt-4'>
-                <MoveLeft />
-                <p className='ml-8'>Please connect your profile on the top left of this window.</p>
-              </div>
-              <Separator className="my-4" />
-            </div>
-          </>}
-          <div className='flex flex-col items-center mt-12 max-w-md text-center'>
-            <h1 className="font-finger-paint text-4xl mb-4">Breathing Life</h1>
-            <p className="">“Breathing Life” is a creative tool for animating images into breath-taking visual experiences. Inspired by altered visionary states the images come to life in a natural way, while it is also possible to create otherworldly effects.</p>
-          </div>
-          {profileData && <>
-            <div className='flex flex-col items-center mt-16'>
-              <div className='flex flex-row items-center justify-center'>
-                <span className='mr-4 text-sm'>Connected as:</span>
-                <Avatar className='h-8 w-8 mr-2'>
-                  <AvatarImage src={profileData.profileImages.length > 0 ? transformIpfsUrl(profileData.profileImages[0].url) : ''} />
-                  <AvatarFallback>{profileData.name.slice(0, 2)}</AvatarFallback>
-                </Avatar>
-                <span className=''>{profileData.name}</span>
-              </div>
-              <div>Get access by minting the access token here.</div>
-              {totalSupply !== undefined && <>
-                <div>Access tokens left: {MAX_SUPPLY - totalSupply}</div>
-              </>}
-            </div>
-          </>}
-        </> : <ShaderExperience />} */}
       </div>
       <div className='fixed bottom-2 left-2 z-10 h-8 flex flex-row items-center'>
         <Link
-          href="https://studioluftbruecke.org"
+          href={process.env.NEXT_PUBLIC_INFO_URL ?? 'https://studioluftbruecke.org'}
           target="_blank"
         >
-          <Image
+          <Button
+            variant={'ghost'}
+            size={'icon'}
+          >
+            <Info size={16} className='text-muted-foreground' />
+          </Button>
+          {/* <Image
             src="/Studio Luftbrücke Full Symbol.jpg"
             alt="Studio Luftbruecke Logo"
             width={50}
             height={50}
             className="rounded-full aspect-square object-cover h-8 w-8"
-          />
+          /> */}
         </Link>
       </div>
       {isMiniApp && <>
         <div className='fixed bottom-2 right-2 z-10 h-8 flex flex-row items-center'>
           {profileData ? <>
             <div className='flex flex-row items-center justify-center text-sm text-muted-foreground'>
-              {/* <span className='mr-4'>Connected as:</span> */}
               {profileData.profileImages.length > 0 && <>
                 <Avatar className='h-6 w-6 mr-2'>
                   <AvatarImage className='object-cover' src={transformIpfsUrl(profileData.profileImages[0].url)} />
